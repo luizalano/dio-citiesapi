4deab2b4e800948807aee4cdb3c6a444a325646e
Luiz Alberto Alano <alano@luigipietro.com.br>
2021-06-03 21:41:50 -0300

    commit 0

---
+++ src/main/java/br/com/luigipietro/citiesapi/CitiesapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luigipietro.citiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesapiApplication.class, args);
+	}
+
+}
